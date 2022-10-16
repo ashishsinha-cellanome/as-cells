fc85be5da04160e6423adbf6024556ddc760de4f
Mehdi Mohseni <mmohseni@cellareye.com>
2022-10-16 12:15:43 -0700

    CV utilities

---
+++ utils/cv_utils.py
@@ -0,0 +1,155 @@
+import cv2
+import numpy as np
+from PIL import Image
+from typing import Tuple, List, Final, Optional, Dict, Union
+
+
+# Utility functions
+# very efficient batch IoU calculation
+# needed for combining detection results from different image crops on
+# the overlapping parts; we use this function instead of
+# torchvision.ops.box_iou(bboxes1, bboxes2) to remove dependency on torch
+def iou_batch(bboxes1: np.ndarray, bboxes2: np.ndarray) -> np.ndarray:
+    """Given Nx4 and Mx4 ndarrays of bounding boxes, compute pairwise IoUs"""
+    # expand dims to allow computing pairwise IoU via outer products (creates NxM below)
+    bboxes1 = np.expand_dims(bboxes1, 1)  # Nx1x4
+    bboxes2 = np.expand_dims(bboxes2, 0)  # 1xMx4
+    # determine the (x, y) coordinates of the intersection rectangle
+    inter_x1s = np.maximum(bboxes1[..., 0], bboxes2[..., 0])  # pairwise max NxM
+    inter_y1s = np.maximum(bboxes1[..., 1], bboxes2[..., 1])  # pairwise max NxM
+    inter_x2s = np.minimum(bboxes1[..., 2], bboxes2[..., 2])  # pairwise min NxM
+    inter_y2s = np.minimum(bboxes1[..., 3], bboxes2[..., 3])  # pairwise min NxM
+    inter_ws = np.maximum(
+        0.0, inter_x2s - inter_x1s
+    )  # pairwise width of intersection rectangle NxM
+    inter_hs = np.maximum(
+        0.0, inter_y2s - inter_y1s
+    )  # pairwise height of intersection rectangle NxM
+    inter_areas = inter_ws * inter_hs  # pairwise intersection area NxM
+    union_areas = (
+        (bboxes1[..., 2] - bboxes1[..., 0]) * (bboxes1[..., 3] - bboxes1[..., 1])
+        + (bboxes2[..., 2] - bboxes2[..., 0]) * (bboxes2[..., 3] - bboxes2[..., 1])
+        - inter_areas
+        + 1e-30
+    )  # pairwise union area NxM
+    return inter_areas / union_areas  # pairwise intersection divided by union (iou) NxM
+
+# a function to return the area of bounding box
+def box_area(box: np.array) -> float:
+    """
+    Args:
+        box (numpy array of size (4,) or (4, 1) or (1, 4) or a 4-tuple or a 4-elements list): The box.
+    Return the area.
+    """
+    return (box[3] - box[1]) * (box[2] - box[0])
+    
+    
+def get_crop_corners(
+    image_width: int,
+    image_height: int,
+    overlap_in_x: int = 80,
+    overlap_in_y: int = 80,
+    input_size: Tuple[int, int] = (1200, 800),
+) -> List[List[int]]:
+    """
+    A function to get the coordinates of the sub-images/crops given the image dimensions, the model input size
+    and the desired overlap between the crops in the x and y dimensions.
+    Args:
+        image_width (int): Input image width.
+        image_height (int): Input image height.
+        overlap_in_x (int): The desired overlap between the crops in x (this is the minimum, some crops may have more
+            than this number of pixels overlap).
+        overlap_in_y (int): The desired overlap between the crops in y.
+        input_size (2-tuple of integers): The model input size in (width, height).
+    Returns:
+        List of 4-tuples or 4-elements integer coordinates of the sub-mages/crops covering the original image.
+    """
+    # the step size for the starting point of each crop in x and y dimension
+    crop_start_step_x: int = input_size[0] - overlap_in_x
+    crop_start_step_y: int = input_size[1] - overlap_in_y
+    crop_corners: List = []
+
+    # overlapping crops
+    for x_start in range(0, image_width - overlap_in_x, crop_start_step_x):
+        for y_start in range(0, image_height - overlap_in_y, crop_start_step_y):
+            # crop coordinates
+            xc_tl = x_start
+            yc_tl = y_start
+            xc_br = x_start + input_size[0]
+            yc_br = y_start + input_size[1]
+            # make sure we always crop the image with the given size
+            # if we get to the boundaries, extend the crop
+            # size inside the image to always get the same size crop
+            # this is not really needed, but help with capturing more
+            # annotations toward the low/right parts of the image
+            if xc_br > image_width:
+                xc_br = image_width
+                xc_tl = xc_br - input_size[0]
+            if yc_br > image_height:
+                yc_br = image_height
+                yc_tl = yc_br - input_size[1]
+
+            crop_coords = [xc_tl, yc_tl, xc_br, yc_br]
+            crop_corners.append(crop_coords)
+
+    return crop_corners
+   
+   
+def show_detections(input_image: Union[Image.Image, np.array], predictions: Dict[str, Union[list, np.array]], label_map: Dict[int, str]):
+    """
+    A function to display the predictions from the YOLO or Mask R-CNN model. 
+    
+    """
+
+    # colors for displaying bounding boxes
+    COLORS: List[Tuple[int, int, int]] = [
+        (0, 0, 255),
+        (0, 255, 0),
+        (255, 0, 0),
+        (255, 0, 255),
+        (0, 255, 255),
+        (255, 255, 0),
+    ]
+    
+    class_ids: List[int] = list(label_map.keys())
+    if isinstance(input_image, np.ndarray):
+        image: np.array = input_image.copy()
+    else:
+        # convert to a numpy array
+        image: np.array = np.array(input_image)
+    
+    # convert to 3-channels
+    if len(image.shape) < 3:
+        image = np.repeat(np.expand_dims(image, axis=2), 3, axis=2)
+    
+    boxes = predictions['boxes']
+    labels = predictions['labels']
+    if 'masks' in predictions:
+        masks = predictions['masks']
+
+    for i in range(len(labels)):
+        # the bounding box
+        (xtl, ytl, xbr, ybr) = boxes[i]
+        if labels[i] not in class_ids:
+            # use black for incorrect label
+            color = (0, 0, 0)
+            text = "Unknown class ID %s" % labels[i]
+        else:
+            color = COLORS[labels[i] % len(COLORS)]
+            text = label_map[labels[i]]
+        
+        if 'masks' in predictions:
+            color_mask = color * np.repeat(np.expand_dims(masks[i], axis=2), 3, axis=2)
+            blended = 0.4 * color_mask
+            blended[color_mask == 0] = image[ytl:ybr, xtl:xbr][color_mask == 0]
+            blended[color_mask > 0] += 0.6 * image[ytl:ybr, xtl:xbr][color_mask > 0]
+
+            # store the blended ROI in the original image
+            image[ytl:ybr, xtl:xbr] = blended.astype(np.uint8)
+        # add the bounding box with yellow color
+        color = (255, 255, 0)
+        cv2.rectangle(image, (xtl, ytl), (xbr, ybr), color, 1)
+        cv2.putText(
+            image, text, (xtl, ytl + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1
+        )
+    return image
