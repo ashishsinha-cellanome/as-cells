82df2bd7b3ecae1729ff3451ee5c80f46dc967ab
Mehdi Mohseni <mmohseni@cellareye.com>
2025-02-03 21:41:57 -0800

    Added a class for normalization

--- Mask RCNN/references/detection/transforms.py
+++ Mask RCNN/references/detection/transforms.py
@@ -65,7 +65,7 @@ class Normalize(nn.Module):
     def forward(
         self, image: Tensor, target: Optional[Dict[str, Tensor]] = None
     ) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]:
-        image = F.normalize(img, mean=self.mean, std=self.std)
+        image = F.normalize(image, mean=self.mean, std=self.std)
         return image, target
 
 class ConvertImageDtype(nn.Module):
