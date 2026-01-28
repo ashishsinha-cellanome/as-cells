05ddee53a633f3a07f906053441519d6a0f15082
Ashish Sinah <ashishsinha108@gmail.com>
2026-01-28 08:47:14 -0700

    update sbatch

--- run_job_infinite.sh
+++ run_job_infinite.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=large_training_job
 #SBATCH --account=aip-robsc
-#SBATCH --nodes=8
+#SBATCH --nodes=3
 #SBATCH --mail-user=ashish.sinha@amii.ca
-#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-type=END,FAIL,BEGIN
 #SBATCH --gpus-per-node=4
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=4
-#SBATCH --exclusive
+#SBATCH --cpus-per-task=2
+##SBATCH --exclusive
 #SBATCH --mem-per-gpu=32G
 #SBATCH --time=7-00:00:00
 #SBATCH --output=logs/%x-%j.out
@@ -34,4 +34,4 @@ echo "Job started at: $(date)"
 echo "Account used: $SLURM_JOB_ACCOUNT"
 
 # srun -o logs/%x-%j-%t.out -e logs/%x-%j-%t.err uv run train_rt_detr.py -m model=rtdetr_v1 model.ema.enabled=True,False model.backbone.train_backbone=True 
-srun  uv run train_rt_detr_v2.py -m data=vulcan   trainer.max_epochs=2 debug=True  model=rtdetr_v1,rtdetr_v2 model/backbone=resnet50  model.ema.enabled=True,False model.backbone.train_backbone=True,False model.backbone.freeze_at_stage=0,1 optimizer.optimizer.use_param_groups=true,false
+srun  uv run train_rt_detr_v2.py -m data=vulcan   trainer.max_epochs=50  model=rtdetr_v1,rtdetr_v2 model/backbone=resnet50  model.ema.enabled=True,False model.backbone.train_backbone=True,False model.backbone.freeze_at_stage=0,1 optimizer.optimizer.use_param_groups=true,false
