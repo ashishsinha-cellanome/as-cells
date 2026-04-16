63bff67c85eb3f9c53f596ec4625fb02f93fd6dc
Ashish Sinha <ashishsinha108@gmail.com>
2026-04-16 15:43:15 -0700

    feat: build spatial prior module for vit adapter

---
+++ models/dinov2_adapter.py
@@ -0,0 +1,56 @@
+import torch
+import torch.nn as nn
+from transformers import Dinov2Model, Dinov2Config
+from transformers import PreTrainedModel
+
+
+class SpatialPriorModule(nn.Module):
+    """CNN stem to extract multi-scale spatial features from the input image."""
+
+    def __init__(self, in_channels=3, embed_dim=768):
+        super().__init__()
+        # Extract Stride 4 (Initial spatial resolution)
+        self.stem = nn.Sequential(
+            nn.Conv2d(in_channels, embed_dim // 4, kernel_size=3, stride=2, padding=1),
+            nn.SyncBatchNorm(embed_dim // 4)
+            if torch.cuda.device_count() > 1
+            else nn.BatchNorm2d(embed_dim // 4),
+            nn.GELU(),
+            nn.Conv2d(
+                embed_dim // 4, embed_dim // 2, kernel_size=3, stride=2, padding=1
+            ),
+            nn.SyncBatchNorm(embed_dim // 2)
+            if torch.cuda.device_count() > 1
+            else nn.BatchNorm2d(embed_dim // 2),
+            nn.GELU(),
+            nn.Conv2d(
+                embed_dim // 2, embed_dim // 2, kernel_size=3, stride=1, padding=1
+            ),
+            nn.SyncBatchNorm(embed_dim // 2)
+            if torch.cuda.device_count() > 1
+            else nn.BatchNorm2d(embed_dim // 2),
+            nn.GELU(),
+        )
+
+        # Stride 8 downsample
+        self.down1 = nn.Sequential(
+            nn.Conv2d(embed_dim // 2, embed_dim, kernel_size=3, stride=2, padding=1),
+            nn.SyncBatchNorm(embed_dim)
+            if torch.cuda.device_count() > 1
+            else nn.BatchNorm2d(embed_dim),
+        )
+
+        # Stride 16 downsample
+        self.down2 = nn.Sequential(
+            nn.Conv2d(embed_dim, embed_dim, kernel_size=3, stride=2, padding=1),
+            nn.SyncBatchNorm(embed_dim)
+            if torch.cuda.device_count() > 1
+            else nn.BatchNorm2d(embed_dim),
+        )
+
+    def forward(self, x):
+        # x is B x 3 x H x W
+        f4 = self.stem(x)  # B x (C/2) x (H/4) x (W/4)
+        f8 = self.down1(f4)  # B x C x (H/8) x (W/8)
+        f16 = self.down2(f8)  # B x C x (H/16) x (W/16)
+        return [f4, f8, f16]
