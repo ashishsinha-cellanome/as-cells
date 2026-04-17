3f10f838524bdbcfd45546b270b1d8c01d818476
Ashish Sinha <ashishsinha108@gmail.com>
2026-04-17 15:51:29 -0700

    perf(mask2former): cast mask_labels to bool in DataLoader to fix 7.6GB OOM
    This prevents PyTorch from allocating massive float32 tensors when moving the ground truth list of masks to the GPU.

--- data/mask2former_data_module.py
+++ data/mask2former_data_module.py
@@ -347,7 +347,7 @@ class Mask2FormerDataset(Dataset):
         return {
             "pixel_values": processed["pixel_values"].squeeze(0),
             "pixel_mask": processed["pixel_mask"].squeeze(0),
-            "mask_labels": processed["mask_labels"][0],
+            "mask_labels": processed["mask_labels"][0].to(torch.bool),
             "class_labels": processed["class_labels"][0],
             "image_id": torch.tensor(image_id, dtype=torch.long),
             "orig_size": torch.tensor([image_height, image_width], dtype=torch.long),
