bea59649bd8fc88b0f923f4360b58e4f81a1140c
Ashish Sinha <ashishsinha108@gmail.com>
2026-04-22 17:58:09 -0700

    rm useless files

--- test_hf_home.py
+++
@@ -1,6 +0,0 @@
-import os
-from huggingface_hub import constants
-
-print("HF cache dir before:", constants.HF_HUB_CACHE)
-os.environ["HF_HOME"] = "/tmp/my_fake_cache"
-print("HF cache dir after setting env:", constants.HF_HUB_CACHE)
