2b88ef275a52625057255f6e8c6b97da2029384e
Ashish Sinha <ashishsinha108@gmail.com>
2026-05-04 23:40:12 -0700

    chore: change num_nodes to 4 and gpus_per_node to 2 for ablation runs

--- run_deimv2_ablation.sh
+++ run_deimv2_ablation.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=deimv2_ablate
 #SBATCH --account=aip-robsc
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --mail-user=ashish.sinha@amii.ca
 #SBATCH --mail-type=END,FAIL,BEGIN
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=2
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-gpu=32G
